ce765948b12e414840ab6223af2cbcc310d948cb
Christian Lopez <cdlopezmorcillo@gmail.com>
2016-03-20 22:29:35 -0500

    Rapido2

---
+++ app/src/main/java/com/lopez/christian/icesi/rapido2_/Pantalla_Principal.java
@@ -0,0 +1,13 @@
+package com.lopez.christian.icesi.rapido2_;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla__principal);
+    }
+}
